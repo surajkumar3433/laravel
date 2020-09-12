895622cfa11fc2effb468f1ea5d810feebd3c322
surajkumar3433 <57474670+surajkumar3433@users.noreply.github.com>
2020-09-12 18:57:59 +0530

    Add files via upload

---
+++ blog/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Http\Controllers\api;
+
+use App\JobApply;
+use App\JobPost;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+    /**
+     * Api is used to add the user.
+     * @method post;
+     * @param string name;
+     * @param string email;
+     * @param string password;
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function AddUser(Request $request)
+    {
+        $data = [];
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        if ($user->save()) {
+            $data['status'] = 200;
+        } else {
+            $data['error'] = 'Data not save';
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * Api is used to create a new job post
+     * @param string title;
+     * @param string description;
+     * @param integer vacancy;
+     * @method post
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function CreatePost(Request $request)
+    {
+        $data = [];
+        $job = new JobPost();
+        $job->title = $request->title;
+        $job->description = $request->description;
+        $job->vacancy = $request->vacancy;
+        $job->state_id = 1;
+        if ($job->save()) {
+            $data['status'] = 200;
+        } else {
+            $data['error'] = 'data not save';
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * User can apply on the post by hitting this api
+     * @param integer job_id;
+     * @method post;
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ApplyPost(Request $request)
+    {
+        $data = [];
+        $model = new JobApply();
+        $model->user_id = $request->user_id;
+        $model->job_id = $request->job_id;
+        if ($model->save()) {
+            $data['status'] = 200;
+        } else {
+            $data['error'] = 'data not save';
+        }
+        return response()->json($data);
+    }
+}
---
+++ blog/database/migrations/2020_09_12_074340_create_job_applies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobAppliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_applies', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('user_id');
+			$table->integer('job_id');
+			$table->integer('state_id')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_applies');
+    }
+}
